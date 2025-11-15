368686900e852db63c0a8d5d3ea9556d737caf0c
iPeerol-Dev <peeroldev@gmail.com>
2025-11-15 04:24:28 +0100

    cv added and updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -38,7 +38,7 @@ const Navbar = () => {
         } hover:text-white text-[20px] font-medium cursor-pointer`}
       >
         <a 
-          href="/cv/Peter_Osho's_Tech_CV.pdf" 
+          href="/mycv/Peter_Osho's_Tech_CV.pdf" 
           download="Peter_Osho's_Tech_CV.pdf"
           aria-label="Download CV"
           className="hover:text-white transition-colors"
